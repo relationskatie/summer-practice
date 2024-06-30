fc7836bd88ad9ea6932052466324f9a6ad1c5fb5
Katie <nurlygayanova2005@gmail.com>
2024-06-30 21:21:50 +0300

    add handlePostForm

--- server/internal/controller/http/hadlers.go
+++ server/internal/controller/http/hadlers.go
@@ -6,6 +6,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/relationskatie/summer-practice/server/client"
+	"github.com/relationskatie/summer-practice/server/internal/model"
 )
 
 func (ctrl *Controller) HandleGetHomePage(c echo.Context) error {
@@ -30,7 +31,24 @@ func (ctrl *Controller) HandleGetForm(c echo.Context) error {
 }
 
 func (ctrl *Controller) HandlePostForm(c echo.Context) error {
-	return nil
+	var (
+		request model.FormRequest
+	)
+	if err := c.Bind(&request); err != nil {
+		return c.JSON(
+			http.StatusBadRequest, err)
+	}
+	params := &model.FormResponse{
+		ID:         uuid.New(),
+		Text:       request.Text,
+		Salary:     request.Salary,
+		Area:       request.Area,
+		URL:        request.URL,
+		Employment: request.Employment,
+		Experience: request.Experience,
+	}
+	return c.JSON(http.StatusCreated, params)
+
 }
 
 func (ctrl *Controller) HandleGetAllVacancies(c echo.Context) error {
--- server/internal/model/requests.go
+++ server/internal/model/requests.go
@@ -1,1 +1,13 @@
 package model
+
+import uuid "github.com/vgarvardt/pgx-google-uuid/v5"
+
+type FormRequest struct {
+	ID         uuid.UUID `json:"id"`
+	Text       string    `json"name"`
+	Salary     string    `json:"salary"`
+	Area       string    `json:"area`
+	URL        string    `json:"url"`
+	Employment string    `json:"employment"`
+	Experience string    `json:"experience"`
+}
